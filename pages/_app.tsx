8da946151b088df33675612ff9473762ac6fa0a9
RJ Dellecese <rj@rjdellecese.com>
2022-09-27 22:45:53 -0400

    Initial Convex setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import clientConfig from "../convex/_generated/clientConfig";
+
+const convex = new ConvexReactClient(clientConfig);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConvexProvider client={convex}>
+      <Component {...pageProps} />
+    </ConvexProvider>
+
+  );
 }
 
-export default MyApp
+export default MyApp;
