6c281a3a17ed205a330c7042fc6383e4618adb12
RJ Dellecese <rj@rjdellecese.com>
2022-10-19 11:46:17 -0400

    Add Sentry, mostly

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
 import { programWithDebuggerWithFlags } from "elm-ts/lib/Debug/Navigation";
 import { programWithFlags } from "elm-ts/lib/Navigation";
 import * as React from "elm-ts/lib/React";
@@ -5,6 +7,16 @@ import { render } from "react-dom";
 
 import * as flags from "~/src/frontend/flags";
 import * as page from "~/src/frontend/page";
+
+Sentry.init({
+  dsn: "https://49a723076cc84925bfb2265827ca1270@o4504010981179392.ingest.sentry.io/4504010983604224",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const program =
   process.env.NODE_ENV === "production"
