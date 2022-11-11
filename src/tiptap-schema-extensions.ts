cfdb179ba42b22656e65beed887b51530dd25452
RJ Dellecese <rj@rjdellecese.com>
2022-11-11 18:50:37 -0500

    Add Typography and Link Tiptap extensions

--- src/tiptap-schema-extensions.ts
+++ src/tiptap-schema-extensions.ts
@@ -1,6 +1,8 @@
 import { getSchema } from "@tiptap/core";
+import Link from "@tiptap/extension-link";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
+import Typography from "@tiptap/extension-typography";
 import Underline from "@tiptap/extension-underline";
 import StarterKit from "@tiptap/starter-kit";
 
@@ -10,6 +12,8 @@ export const extensions = [
   TaskList,
   TaskItem.configure({ nested: true }),
   Underline,
+  Typography,
+  Link,
 ];
 
 export const schema = getSchema(extensions);
