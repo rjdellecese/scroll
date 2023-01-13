5bfa537d113b2e676250d20745f5defdd34c5166
RJ Dellecese <rj@rjdellecese.com>
2023-01-13 22:26:19 +0100

    Increase footer's bottom padding

---
+++ src/elm-ts/tailwind-config.ts
@@ -0,0 +1,15 @@
+import resolveConfig from "tailwindcss/resolveConfig";
+import type { Config } from "tailwindcss/types/config";
+import type { DefaultColors } from "tailwindcss/types/generated/colors";
+import type { DefaultTheme } from "tailwindcss/types/generated/default-theme";
+
+// @ts-ignore
+import tailwindConfig from "~tailwind.config.js";
+
+// eslint-disable-next-line no-type-assertion/no-type-assertion
+export default resolveConfig(tailwindConfig) as Config & {
+  theme: DefaultTheme & {
+    width: DefaultTheme["spacing"];
+    colors: DefaultColors;
+  };
+};
